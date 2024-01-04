3f2c5702ddc267efb48253cd70f6aeedc0d0f706
Patrick <73622070+holycactusjuice@users.noreply.github.com>
2024-01-04 16:51:20 -0500

    updated server.py and removed redudant app.py

--- server/server.py
+++ server/server.py
@@ -2,12 +2,5 @@ from website import create_app
 
 app = create_app()
 
-
-@app.route("/members")
-# members api route
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
-
-
 if __name__ == "__main__":
     app.run(debug=True)

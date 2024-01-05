8cbbb7aecf06cf35275bb328b449cc4822c3af2d
Patrick <73622070+holycactusjuice@users.noreply.github.com>
2024-01-05 09:43:14 -0500

    typo

--- server/website/__init__.py
+++ server/website/__init__.py
@@ -13,8 +13,8 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
 MONGODB_KEY = os.getenv('MONGODB_KEY')
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
 MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 
 
@@ -22,7 +22,7 @@ client = MongoClient(
     f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.iruvwvi.mongodb.net/test?retryWrites=true&w=majority')
 db = client[DB_NAME]
 users = db['users']
-trakcs = db['tracks']
+tracks = db['tracks']
 
 
 def create_app():

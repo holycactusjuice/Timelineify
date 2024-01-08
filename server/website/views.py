d42e4c62d52b602ecaadcee399832afc56116957
Patrick <73622070+holycactusjuice@users.noreply.github.com>
2024-01-08 13:56:24 -0500

    route for top tracks playlist

--- server/website/views.py
+++ server/website/views.py
@@ -1,14 +1,10 @@
-from flask import redirect, Blueprint, render_template, url_for, session
+from flask import redirect, Blueprint, render_template, url_for, session, jsonify
 from flask_login import login_required, login_user, logout_user
 
 from .models import User
+from .spotify import Spotify
 
 views = Blueprint("views", __name__)
-
-
-@views.route("/")
-def index():
-    return f"user_id: {session.get('user_id')}"
 
 
 @views.route("/user-data")
@@ -19,4 +15,34 @@ def user_data():
     # remove _id since it is not JSON serializable and not needed
     user_doc.pop("_id", None)
 
-    return user_doc
+    return jsonify(user_doc)
+
+
+@views.route("/top-tracks-playlist/<month>/<length>")
+def top_tracks_playlist(month, length):
+    # cast month and length to correct types
+    month = str(month)
+    length = int(length)
+
+    user_id = session.get('user_id')
+    # retrieve user info from MongoDB using the user_id from the session
+    user = User.get(user_id)
+
+    playlist_name = f"Your {month} top {length}"
+    description = f"Your top {length} songs for {month}"
+
+    playlist_id = user.create_top_tracks_playlist(
+        month, length, playlist_name, description)
+
+    return jsonify({"playlist_id": playlist_id})
+
+
+@views.route("/overview-data")
+def overview_data():
+    user_id = session.get('user_id')
+    # retrieve user info from MongoDB using the user_id from the session
+    user = User.get(user_id)
+
+    overview_data = user.get_overview_data()
+
+    return jsonify(overview_data)
